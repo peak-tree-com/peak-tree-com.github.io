30fb601599938fb5c9728d32560e72983f306a65
vk-develops <vimalkumarv291@gmail.com>
2023-08-27 14:23:02 +0530

    initial commit for the projects section

--- Website/JS/app.js
+++ Website/JS/app.js
@@ -12,3 +12,33 @@ function hideMenu() {
 //     smooth: true
 // });
 // const scroll = new LocomotiveScroll(".main");
+
+function navScroller(){
+    const body = document.getElementById("Header");
+    let lastScroll = 0;
+
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset;
+
+        if(currentScroll <= 0){
+            body.classList.remove("scroll-up");
+        }
+
+        if(currentScroll > lastScroll && !body.classList.contains("scroll-down")){
+            body.classList.remove("scroll-up");
+            body.classList.add("scroll-down");
+        }
+
+        if(currentScroll < lastScroll && body.classList.contains("scroll-down")){
+            body.classList.add("scroll-up");
+            body.classList.remove("scroll-down");
+        }
+
+
+
+        lastScroll = currentScroll;
+    });
+
+}
+
+// navScroller();
